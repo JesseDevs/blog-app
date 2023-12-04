f4f61733fb9382f025bcfaa5947ee51910359856
Jesse <jessemerge2@gmail.com>
2023-12-03 23:26:18 -0800

    Got sign up and sign in working properly

--- pages/register.vue
+++ pages/register.vue
@@ -1,10 +1,9 @@
 <script setup>
 	const credentials = reactive({
-		firstName: '',
-		surname: '',
+		fullName: '',
+		username: '',
 		email: '',
 		password: '',
-		passwordRepeat: '',
 		successMessage: '',
 		errorMessage: '',
 	});
@@ -13,88 +12,197 @@
 	const router = useRouter();
 	const user = useSupabaseUser();
 	async function register() {
-		const { firstName, surname, email, password, passwordRepeat } = credentials;
-		if (password !== passwordRepeat) return;
+		const { fullName, username, email, password } = credentials;
 		const { error } = await client.auth.signUp({
 			email,
 			password,
 			options: {
 				data: {
-					first_name: firstName,
-					surname,
+					full_name: fullName,
+					username,
 					email,
 				},
 				emailRedirectTo: 'http://localhost:3000/',
 			},
 		});
+
 		if (error) {
-			alert('Something went wrong !');
+			if (error.message.includes('unique constraint')) {
+				credentials.errorMessage = 'Email or username already exists.';
+			} else {
+				credentials.errorMessage = 'Registration failed. Please try again.';
+			}
+			console.error(error);
 			return;
 		}
-		alert('Open the email we sent you to verify your account!');
+		isModalOpen.value = true;
 	}
+
+	const isModalOpen = ref(false);
+	const closeModal = () => {
+		isModalOpen.value = false;
+	};
+
+	const showPassword = ref(false);
+	const togglePassword = () => {
+		if (credentials.password) {
+			showPassword.value = !showPassword.value;
+		}
+	};
 </script>
 
 <template>
 	<section class="register">
 		<inner-column>
-			<form @submit.prevent="register()">
-				<div class="field">
-					<label for="first-name">First Name</label>
-					<input type="text" id="first-name" v-model="credentials.firstName" />
+			<signup-page>
+				<div>
+					<h2 class="level-three-voice">Sign Up</h2>
+					<p class="sub-head">Get started with an account</p>
 				</div>
-				<div class="field">
-					<label for="surname">Last Name</label>
-					<input type="text" id="surname" v-model="credentials.surname" />
+				<form @submit.prevent="register()">
+					<div class="field">
+						<label for="email">Email<span>*</span></label>
+						<input
+							id="email"
+							required
+							type="email"
+							placeholder="Email"
+							v-model="credentials.email"
+							aria-label="Email"
+						/>
+					</div>
+					<div class="field">
+						<label for="fullName">Full Name</label>
+						<input
+							type="text"
+							id="fullName"
+							placeholder="Full Name"
+							v-model="credentials.fullName"
+							aria-label="Full Name"
+						/>
+					</div>
+					<div class="field">
+						<label for="username">Username</label>
+						<input
+							type="text"
+							id="username"
+							placeholder="Username"
+							v-model="credentials.username"
+							aria-label="Username"
+						/>
+					</div>
+					<div class="field">
+						<label for="password">Password<span>*</span></label>
+						<input
+							id="password"
+							required
+							:type="showPassword ? 'text' : 'password'"
+							placeholder="Enter your password"
+							v-model="credentials.password"
+							autocomplete="off"
+							aria-label="Password"
+						/>
+						<button class="showBTN" @click.prevent="togglePassword">
+							<Icon
+								:name="`${showPassword ? 'mi:eye' : 'mi:eye-off'}`"
+								color="rgb(128, 127, 127)"
+								size="28"
+							/>
+						</button>
+					</div>
+					<p class="form-support small-voice">
+						By signing up, you agree to the
+						<a class="small-voice" href="/guide/terms">Terms of Service</a>
+						and
+						<a class="small-voice" href="/guide/privacy">Privacy Policy</a>
+					</p>
+					<button class="button-filled" type="submit">Submit</button>
+				</form>
+				<div class="error" v-if="credentials.errorMessage">
+					<p>{{ credentials.errorMessage }}</p>
 				</div>
-				<div class="field">
-					<label for="email">Email</label>
-					<input id="email" type="email" v-model="credentials.email" />
-				</div>
+				<glass-container
+					v-if="credentials.successMessage && isModalOpen"
+					:class="{ success: isModalOpen }"
+				>
+					<h3 class="level-two-voice">Confirmation</h3>
+					<button @click="closeModal">Close</button>
+					<p>{{ credentials.successMessage }}</p>
+				</glass-container>
 
-				<div class="field">
-					<label for="password">Password</label>
-					<input
-						id="password"
-						type="password"
-						v-model="credentials.password"
-						autocomplete="off"
-					/>
-				</div>
-				<div class="field">
-					<label for="repeat-password">Password</label>
-					<input
-						id="repeat-password"
-						type="password"
-						v-model="credentials.passwordRepeat"
-						autocomplete="off"
-					/>
-				</div>
-
-				<button type="submit">Submit</button>
-			</form>
-
-			<div class="success" v-if="credentials.successMessage">
-				{{ credentials.successMessage }}
-			</div>
-
-			<div class="error" v-if="credentials.errorMessage">
-				{{ credentials.errorMessage }}
-			</div>
+				<div v-if="user">{{ user.email }}</div>
+			</signup-page>
 		</inner-column>
 	</section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+	section {
+		height: 100vh;
+	}
+	signup-page {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		gap: 2rem;
+
+		.sub-head {
+			opacity: 0.75;
+		}
+	}
 	form {
-		display: grid;
-		gap: 20px;
+		display: flex;
+		flex-direction: column;
 		max-width: 400px;
 		justify-items: start;
+
+		.field {
+			display: flex;
+			margin-bottom: 15px;
+			flex-direction: column;
+			width: 100%;
+			gap: 5px;
+			position: relative;
+
+			&:last-of-type {
+				margin-bottom: 8px;
+			}
+			input {
+				font-family: 'Roboto Slab', serif;
+				width: 100%;
+				padding: 8px 1rem;
+				font-size: var(--size-base);
+				background-color: var(--light-fade);
+				border: none;
+				outline: none;
+				&::placeholder {
+					color: rgb(101, 101, 101);
+				}
+				&:focus {
+					outline: 1px solid rgb(128, 127, 127);
+				}
+			}
+		}
+
+		.form-support {
+			margin-bottom: 30px;
+			a {
+				font-size: inherit;
+				text-decoration: underline;
+			}
+		}
 	}
 
-	.field {
-		display: grid;
+	.showBTN {
+		background: transparent;
+		color: black;
+		position: absolute;
+		bottom: 7px;
+		right: 5px;
+		padding: 0 5px;
+		border: none;
+		outline: none;
+		cursor: pointer;
 	}
 
 	.success {
