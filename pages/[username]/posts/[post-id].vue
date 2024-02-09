006bfca5c8d0da2008e99c5dee45e3c199509879
Jesse <jessemerge2@gmail.com>
2024-02-09 02:42:47 -0800

    Work on detail route

--- pages/[username]/posts/[post-id].vue
+++ pages/[username]/posts/[post-id].vue
@@ -1,9 +1,23 @@
 <template>
-	<detail-page>
-		detail
-		<h1>{{ post.header }}</h1>
-		<p>{{ post.content }}</p>
-	</detail-page>
+	<section class="detail">
+		<inner-column>
+			<detail-page>
+				<picture>
+					<img
+						:src="`https://naduzuobtmmhavkozjxf.supabase.co/storage/v1/object/public/post-images/${post.belongs_to}/${post.image_url}?t=2024-01-30T08%3A07%3A46.058Z`"
+						alt=""
+						@error="handleImageError"
+						@load="imageLoaded = true"
+						loading="lazy"
+					/>
+					<div class="vignette-overlay"></div>
+				</picture>
+
+				<h1>{{ post.header }}</h1>
+				<p>{{ post.content }}</p>
+			</detail-page>
+		</inner-column>
+	</section>
 </template>
 
 <script setup>
@@ -37,5 +51,28 @@
 		flex-direction: column;
 		width: 100%;
 		position: relative;
+
+		picture {
+			position: relative;
+
+			img {
+				object-fit: cover;
+			}
+
+			.vignette-overlay {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				background: rgb(255, 255, 255);
+				background: radial-gradient(
+					circle,
+					rgba(255, 255, 255, 0) 0%,
+					rgba(0, 0, 0, 0.6) 90%
+				);
+				pointer-events: none;
+			}
+		}
 	}
 </style>
