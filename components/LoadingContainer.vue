7960e1f5c09306a1336d0f41656bb112ee8dca8a
Jesse <jessemerge2@gmail.com>
2024-02-27 23:09:23 -0800

    Change loading block into own component

---
+++ components/LoadingContainer.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="loading-container">
+		<div>
+			<text-content>
+				<p class="level-two-voice" v-html="text"></p>
+			</text-content>
+			<Icon name="line-md:loading-twotone-loop" size="100" />
+		</div>
+	</div>
+</template>
+
+<script setup>
+	const props = defineProps({
+		text: String,
+	});
+</script>
+
+<style lang="scss" scoped></style>
