7dfa9d60f15c88d05333691295b1e8f178e4a8cb
Jesse <jessemerge2@gmail.com>
2024-03-08 17:44:05 -0800

    Adjust home card to use in multiple places

--- components/ExploreCards.vue
+++ components/ExploreCards.vue
@@ -1,9 +1,12 @@
 <template>
-	<ul v-if="posts" class="card-menu" id="explore-cards">
-		<li v-for="post in posts" :key="post.id">
-			<PostHomeCard :post="post" />
-		</li>
-	</ul>
+	<div>
+		<ul v-if="posts" class="card-menu" id="explore-cards">
+			<li v-for="post in posts" :key="post.id">
+				<PostHomeCard :post="post" />
+			</li>
+		</ul>
+		<button @click="loadMorePosts">Load More</button>
+	</div>
 </template>
 
 <script setup>
@@ -12,6 +15,10 @@
 	const user = useSupabaseUser();
 	const route = useRoute();
 	const username = ref('');
+
+	const pageSize = 10; // Number of posts to load per page
+	let currentPage = ref(1); // Current page of loaded posts
+	let hasMorePosts = ref(true);
 
 	const fetchUsernameById = async (id) => {
 		try {
@@ -41,15 +48,31 @@
 			const { data, error } = await client
 				.from('posts')
 				.select('*')
-				.order('created_at', { ascending: false });
+				.order('created_at', { ascending: false })
+				.range(
+					(currentPage.value - 1) * pageSize,
+					currentPage.value * pageSize - 1,
+				);
 
 			if (error) {
 				console.error('Error fetching posts:', error.message);
 			} else {
-				posts.value = data;
+				if (data.length < pageSize) {
+					hasMorePosts.value = false; // No more posts to load
+				}
+				posts.value.push(...data);
+				currentPage.value++;
 			}
 		} catch (error) {
 			console.error('Error fetching posts:', error.message);
+		}
+	};
+
+	const loadMorePosts = async () => {
+		await fetchPosts();
+		for (const post of posts.value) {
+			const user = await getUserByUsername(post.belongs_to);
+			post.username = user; // Add username property to each post
 		}
 	};
 
@@ -67,7 +90,7 @@
 		await fetchPosts();
 		for (const post of posts.value) {
 			const user = await getUserByUsername(post.belongs_to);
-			post.username = user; // Add username property to each post
+			post.username = user;
 		}
 	});
 </script>
